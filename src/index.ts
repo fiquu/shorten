39ea925e7f6011e0d3d22b32760622a156be77df
Santiago G. Marín <santiago.marin@dotstudio.io>
2020-02-10 12:01:23 -0300

    Completed implementation

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,14 @@
  */
 export interface ShortenOptions {
   /**
-   * String to use as ellipsis.
-   */
-  ellipsis?: string;
-
-  /**
    * Maximum string length.
    */
   length?: number;
+
+  /**
+   * String to use as ellipsis.
+   */
+  ellipsis?: string;
 
   /**
    * Whether to keep whole words.
@@ -18,9 +18,51 @@ export interface ShortenOptions {
   words?: boolean;
 
   /**
-   * Whether to count ellipsis's length in the maximum length.
+   * Whether to count ellipsis' length in the maximum length.
    */
   lax?: boolean;
+}
+
+/**
+ * Shorten default options.
+ */
+const defaults: ShortenOptions = Object.freeze<ShortenOptions>({
+  ellipsis: '...',
+  words: true,
+  length: 50,
+  lax: false
+});
+
+/**
+ * Trims a string to the provided length.
+ *
+ * @param {string} value The value to trim.
+ * @param {object} options The shorten options object.
+ *
+ * @returns {string} The trimmed string.
+ */
+function trimToLength(value: string, options?: ShortenOptions): string {
+  const { ellipsis, lax, length } = options;
+  const pad: number = lax ? 0 : ellipsis.length;
+
+  return value.substr(0, length - pad);
+}
+
+/**
+ * Trims a string to words.
+ *
+ * @param {string} value The value to trim.
+ * @param {object} options The shorten options object.
+ *
+ * @returns {string} The trimmed string.
+ */
+function trimToWords(value: string, options?: ShortenOptions): string {
+  const { ellipsis } = options;
+  const lastSpace: number = value.lastIndexOf(' ');
+  const index: number = Math.min(value.length, lastSpace);
+  const adjusted: string = value.substr(0, index);
+
+  return `${adjusted}${ellipsis}`;
 }
 
 /**
@@ -28,37 +70,41 @@ export interface ShortenOptions {
  * default.
  *
  * @param {string} value The string to shorten.
- * @param {ShortenOptions} options The options object.
+ * @param {object} options The options object.
+ * @param {number} options.length Maximum length to return. Defaults to `50`.
+ * @param {string} options.ellipsis Ellipsis string to use. Defaults to `'...'`.
+ * @param {boolean} options.words Whether to keep words. Defaults to `true`.
+ * @param {boolean} options.lax Whether to not count the ellipsis string
+ * length in the maximum length. Defaults to `false`.
  *
- * @returns {string} The truncated string.
+ * @example
+ * // Returns 'The...'
+ * shorten('The strnig is logn and full of erorrs.');
+ * @example
+ * // Returns 'The strnig is...'
+ * shorten('The strnig is logn and full of erorrs.', { length: 20 });
+ *
+ * @returns {string} The shortened (truncated) string.
  */
-function shorten(value: string, options: ShortenOptions = {}): string {
-  const { length = 10, ellipsis = '...', words = true, lax = false } = {
+export default function shorten(value: string, options?: ShortenOptions): string {
+  const _opts: ShortenOptions = Object.freeze<ShortenOptions>({
+    ...defaults,
     ...options
-  };
+  });
 
   if (!value) {
     return '';
   }
 
-  if (value.length <= length) {
+  if (value.length <= _opts.length) {
     return value;
   }
 
-  // Trim the string to the maximum length
-  const pad: number = lax ? 0 : ellipsis.length;
-  const trimmed: string = value.substr(0, length - pad);
+  const trimmed = trimToLength(value, _opts);
 
-  if (!words) {
-    return `${trimmed}${ellipsis}`;
+  if (_opts.words) {
+    return trimToWords(trimmed, _opts);
   }
 
-  // Adjust if we are in the middle of a word
-  const lastSpace: number = trimmed.lastIndexOf(' ');
-  const index: number = Math.min(trimmed.length, lastSpace);
-  const adjusted: string = trimmed.substr(0, index);
-
-  return `${adjusted}${ellipsis}`;
+  return `${trimmed}${_opts.ellipsis}`;
 }
-
-export default shorten;
